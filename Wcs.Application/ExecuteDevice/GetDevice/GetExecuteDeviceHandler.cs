2612d395a4aaf96b82b1f6a3395b607d04ec8462
吴 <@>
2025-07-22 15:47:29 +0800

    提交

--- Wcs.Application/ExecuteDevice/GetDevice/GetExecuteDeviceHandler.cs
+++ Wcs.Application/ExecuteDevice/GetDevice/GetExecuteDeviceHandler.cs
@@ -3,6 +3,10 @@ using Wcs.Contracts.WcsTask;
 
 namespace Wcs.Application.GetDevice;
 
+
+/// <summary>
+/// 获取执行设备
+/// </summary>
 internal class GetExecuteDeviceHandler : ICommandHandler<GetExecuteDeviceEvent, WcsTaskDto>
 {
     public Task<WcsTaskDto> Handle(GetExecuteDeviceEvent request, CancellationToken cancellationToken)
