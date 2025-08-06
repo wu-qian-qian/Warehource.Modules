1629f4dba5380e657be64f946c5a99bafa839893
吴 <@>
2025-08-06 14:52:51 +0800

    1

---
+++ Common.Application/MediatR/Behaviors/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Application.MediatR.Behaviors
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+
+        public T Value { get;set; }
+
+        public string Message { get; set; }
+
+        public void SetValue(T value)
+        {
+            Value = value;
+            IsSuccess = true;
+        }
+
+        public void SetMessage(string value)
+        {
+            Message = value;
+            IsSuccess = false;
+        }
+    }
+}
---
+++ Plc.Presentation/PlcCommunication/WritePlc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plc.Presentation.PlcCommunication
+{
+    internal class WritePlc
+    {
+    }
+}
